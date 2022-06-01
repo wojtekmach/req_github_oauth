2ab8b2c3708bd34090337e395f56ed1534d9e797
David Bernheisel <dbernheisel@users.noreply.github.com>
2022-06-01 09:40:48 -0400

    Set cached token file to user-only readable (#1)

--- test/req_github_oauth_test.exs
+++ test/req_github_oauth_test.exs
@@ -1,10 +1,19 @@
 defmodule ReqGitHubOAuthTest do
   use ExUnit.Case, async: true
 
-  test "it works" do
-    req = Req.new(http_errors: :raise) |> ReqGitHubOAuth.attach()
+  @moduletag :tmp_dir
+
+  test "it works", %{tmp_dir: tmp_dir} do
+    opts = [gh_token_cache_fs_path: tmp_dir]
+    req = Req.new(http_errors: :raise) |> ReqGitHubOAuth.attach(opts)
 
     Req.get!(req, url: "https://api.github.com/user").body["login"]
     |> IO.inspect()
+
+    cached_token = Path.join(tmp_dir, "token")
+    file_stat = File.stat!(cached_token)
+    assert file_stat.mode == 0o00100600
+  after
+    File.rm_rf(tmp_dir)
   end
 end
